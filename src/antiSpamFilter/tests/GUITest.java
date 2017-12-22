6c488fe253f37eea461b476854148d61e711a418
lmaco <lmacoiscte-iul.pt>
2017-12-22 01:42:43 +0000

    Correção de erros.

--- src/antiSpamFilter/tests/GUITest.java
+++ src/antiSpamFilter/tests/GUITest.java
@@ -8,40 +8,14 @@ public class GUITest {
 
 	@Test
 	public void test() {
-		
-		/**
-		 * Criação de uma nova GUI
-		 */
 		GUI gui = new GUI();
-		
-		/**
-		 * Abertura da GUI
-		 */
 		gui.open();
 		
-		/**
-		 * Carrega as Regras
-		 */
 		gui.carregarRegras();
-		
-		/**
-		 * Avalia o Filtro Manual
-		 */
 		gui.avaliaFiltroManual();
-		
-		/**
-		 * Guarda as Regras Manuais
-		 */
 		gui.guardarRegrasManual();
 		
-		/**
-		 * Avalia o Filtro Automático
-		 */
 		gui.avaliaFiltroAutomatico();
-		
-		/**
-		 * Guarda as Regras Automáticas
-		 */
 		gui.guardarRegrasAutomatico();
 	}
 
--- src/antiSpamFilter/tests/MailTest.java
+++ src/antiSpamFilter/tests/MailTest.java
@@ -13,19 +13,9 @@ public class MailTest {
 	@Test
 	public void test() {
 		
-		/**
-		 * ArrayList de Strings, listaConteudo
-		 */
 		ArrayList<String> listaConteudo = new ArrayList<String>();
-		
-		/**
-		 * Adiciona à listaConteudo "BAYES_00"
-		 */
 		listaConteudo.add("BAYES_00");
 		
-		/**
-		 * Mail com conteudo "0016d82758030e304d41fb3f4ebbb7d9dd91"
-		 */
 		Mail mail = new Mail(true, "0016d82758030e304d41fb3f4ebbb7d9dd91", listaConteudo);
 		
 		Assert.assertEquals("0016d82758030e304d41fb3f4ebbb7d9dd91", mail.getToken());
@@ -33,19 +23,8 @@ public class MailTest {
 		Assert.assertNotNull(mail.getListaConteudo());
 		Assert.assertEquals(1, mail.getListaConteudo().size());
 		
-		/**
-		 * ArrayList de Regras, listaRegras
-		 */
 		ArrayList<Regra> listaRegras = new ArrayList<>();
-		
-		/**
-		 * Adicona à listaRegras a Regra "BAYES_00", com peso 5
-		 */
 		listaRegras.add(new Regra("BAYES_00", 5));
-		
-		/**
-		 * Adicona à listaRegras a Regra "BAYES_007", com peso -5
-		 */
 		listaRegras.add(new Regra("BAYES_007", -5));
 		double somatorioPesos = mail.somaPesos(listaRegras);
 		Assert.assertEquals(5.0, somatorioPesos, 0.00001);
