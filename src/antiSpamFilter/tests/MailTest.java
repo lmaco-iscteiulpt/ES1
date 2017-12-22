b597a31ed270941fad36aac641e3a99fbef2f224
cmrgs1-iscteiulpt <cmrgs1@iscte-iul.pt>
2017-12-22 00:35:32 +0000

    Junit teste da classe mail

---
+++ src/antiSpamFilter/tests/MailTest.java
@@ -0,0 +1,14 @@
+package antiSpamFilter.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MailTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
