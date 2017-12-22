68b1ea76ea8a4f88a555bbe3e6cc0d8f4b52fd4b
lmaco <lmacoiscte-iul.pt>
2017-12-22 01:23:47 +0000

    Criação do JavaDoc da classe Mail.

--- src/antiSpamFilter/Mail.java
+++ src/antiSpamFilter/Mail.java
@@ -2,31 +2,65 @@ package antiSpamFilter;
 
 import java.util.ArrayList;
 
-
 public class Mail {
 
+	/**
+	 * Boolean isSpam
+	 */
 	private boolean isSpam;
+	
+	/**
+	 * String token
+	 */
 	private String token;
+	
+	/**
+	 * ArrayList listaConteudo
+	 */
 	private ArrayList<String> listaConteudo;
 	
+	/**
+	 * Construtor da Classe Mail
+	 * @param isSpam
+	 * @param token
+	 * @param listaConteudo
+	 */
 	public Mail(boolean isSpam, String token, ArrayList<String> listaConteudo) {
 		this.isSpam = isSpam;
 		this.token = token;
 		this.listaConteudo = listaConteudo;
 	}
 	
+	/**
+	 * Devolve o Boolean isSpam
+	 * @return isSpam
+	 */
 	public boolean isSpam() {
 		return isSpam;
 	}
 	
+	/**
+	 * Devolve o String token
+	 * @return token
+	 */
 	public String getToken() {
 		return token;
 	}
 	
+	/**
+	 * Devolve a lista listaConteudo
+	 * @return listaConteudo
+	 */
 	public ArrayList<String> getListaConteudo() {
 		return listaConteudo;
 	}
 	
+	/**
+	 * Soma dos pesos das regras
+	 * 
+	 * @param listaRegras
+	 * @return somatorioPesos
+	 */
 	public double somaPesos(ArrayList<Regra> listaRegras) {
 		double somatorioPesos = 0;
 		for (String palavraConteudo : listaConteudo) {
